e10bd6b9056314003bcaae9ac22b01f3f74093a3
hyundai <dwayne.shim@gmail.com>
2019-03-21 10:40:35 +0900

    implementing klay4py ...

--- pyKlay/klay.py
+++ pyKlay/klay.py
@@ -1,39 +1,26 @@
 import os
 
-from py4j.java_gateway import JavaGateway, GatewayParameters
-from py4j.java_gateway import launch_gateway
+from pyKlay import jvm
 
-__all__ = ['gateway']
+__all__ = ['Klay']
 
-"""gateway = None
+class Klay:
 
-def init_gateway(jar_path="./libs", max_heap=1024):
-    global gateway
+    def __init__(self, config_file_path='./data/configuration/klay.conf'):
 
-    if gateway is not None:
-        return
-"""
-libraries = [
-    '{0}{1}klay-python-wrapper-0.3.jar'
-]
-jar_path = './libs'
-max_heap = 1024
+        jvm.init_jvm()
+        self._klay_ep = jvm.get_jvm().klay.python.wrapper.KlayEntryPoint(config_file_path)
 
-classpath = os.pathsep.join([lib.format(jar_path, os.sep) for lib in libraries])
-py4j_path = '{0}{1}py4j0.10.8.1.jar'.format(jar_path, os.sep)
 
-port = launch_gateway(jarpath=py4j_path,
-                      classpath=classpath,
-                      javaopts=['-Dfile.encoding=UTF8', '-ea', '-Xmx{}m'.format(max_heap)],
-                      die_on_exit=True)
+    def do_klay(self, sentence):
+        return self._klay_ep.doKlay(sentence)
 
-try:
-    gateway = JavaGateway(gateway_parameters=GatewayParameters(port=port, auto_convert=True))
-except Exception as e:
-    gateway = None
 
-config_file_path = './data/configuration/klay.conf'
-klay_ep = gateway.jvm.klay.python.wrapper.KlayEntryPoint(config_file_path)
-morphs = klay_ep.doKlay('안녕하세요 KLAY입니다.')
-for morph in morphs.iterator():
-    print(morph)
+if __name__ == '__main__':
+    klay = Klay()
+
+    text = '① 대한민국은 민주공화국이다. ② 대한민국의 주권은 국민에게 있고, 모든 권력은 국민으로부터 나온다.'
+
+    morphs = klay.do_klay(text)
+    for morph in morphs.iterator():
+        print(morph)
